bb73381d9c41a40314db38d371e68a2c1ea8ead5
Radian Gondokaryono <radian.gondokaryono@dorabot.com>
2022-01-15 00:08:02 -0500

    Test teleop to use unittest framework, but not really assert test anything

--- dvrk_planning/tests/test_teleop.py
+++ dvrk_planning/tests/test_teleop.py
@@ -1,4 +1,5 @@
-from subprocess import call
+import unittest
+
 from PyKDL import Rotation, Vector
 import numpy as np
 
@@ -8,49 +9,104 @@ from dvrk_planning.kinematics.psm import compute_fk
 np.set_printoptions(precision=3)
 np.set_printoptions(suppress=True)
 
-def callback_function(js):
-    print("output joint state to send: \n", js)
-    print("output transform result: \n", compute_fk(js))
+class TestSetup(unittest.TestCase):
+    def setUp(self):
+        self.joint_pos = [0.0, 0.0, 0.1, 0.0, 0.0, 0.0]
+        self.output_start_tf = compute_fk(self.joint_pos)
+        self.inc_x = 0.01
+        self.steps_num = 5
+        print("--- TestSetup ---")
+        print("output start transform: \n", self.output_start_tf)
 
-joint_pos = [0.0, 0.0, 0.1, 0.0, 0.0, 0.0]
-output_start_tf = compute_fk(joint_pos)
-inc_x = 0.01
+class TestIncrementTeleopController(TestSetup):
+    def callback_function(self, js):
+        print("output joint state to send: \n", js)
+        print("output transform result: \n", compute_fk(js))
 
-def test_itc():
-    itc = IncrementTeleopController()
-    itc.register(callback_function)
+    def setUp(self):
+        super().setUp()
+        self.itc = IncrementTeleopController()
+        self.itc.register(self.callback_function)
 
-    print("output start transform: \n", output_start_tf)
-    itc.enable(output_start_tf)
+        print("--- TestIncrementTeleopController ---")
 
-    print("increment step: \n", inc_x)
-    print("Incrementing x --- \n")
-    for _ in range(5):
-        itc.update(Vector(inc_x, 0.0, 0.0), Rotation.Quaternion(0.0, 0.0, 0.0, 1.0))
+    def test_increment_pos_x(self):
+        print("--- test_increment_pos_x ---")
+        self.itc.enable(self.output_start_tf)
+        for _ in range(self.steps_num):
+            self.itc.update(Vector(self.inc_x, 0.0, 0.0), Rotation.Quaternion(0.0, 0.0, 0.0, 1.0))
+        self.itc.disable()
 
-    print("Incrementing rot x --- \n")
-    for _ in range(5):
-        itc.update(Vector(0.0, 0.0, 0.0), Rotation.RPY(0.1, 0.0, 0.0))
+    def test_increment_rot_x(self):
+        print("--- test_increment_rot_x ---")
+        inc_rot_x = 1.5708/2.0 #45 deg
+        self.itc.enable(self.output_start_tf)
+        for _ in range(self.steps_num):
+            self.itc.update(Vector(0.0, 0.0, 0.0), Rotation.RPY(inc_rot_x, 0.0, 0.0))
+        self.itc.disable()
 
-def test_ftc():
-    ftc = FollowTeleopController()
-    ftc.register(callback_function)
-    print("output start transform: \n", output_start_tf)
+    def test_with_camera_frame_pos_x(self):
+        print("--- test_with_camera_frame_pos_x ---")
+        camera_rot = Rotation.RPY(0.0, 0.0, 1.5708) # x becomes y
+        # we should see increment in y now, for an increment in input x
+        itc = IncrementTeleopController(output_to_camera_rot = camera_rot)
+        itc.register(self.callback_function)
+        itc.enable(self.output_start_tf)
+        for _ in range(self.steps_num):
+            itc.update(Vector(self.inc_x, 0.0, 0.0), Rotation.RPY(0.0, 0.0, 0.0))
+        itc.disable()
 
-    input_start_tf = np.identity(4)
-    print("input start transform: \n", input_start_tf)
+class TestFollowTeleopController(TestSetup):
+    def callback_function(self, js):
+        print("output joint state to send: \n", js)
+        print("output transform result: \n", compute_fk(js))
 
-    ftc.enable(input_start_tf, output_start_tf)
-    print("increment step: \n", inc_x)
+    def setUp(self):
+        super().setUp()
+        self.ftc = FollowTeleopController()
+        self.ftc.register(self.callback_function)
+        print("--- TestFollowTeleopController ---")
 
-    print("Incrementing x --- \n")
-    for _ in range(5):
-        input_start_tf[0,3] = input_start_tf[0,3] + inc_x
-        ftc.update(input_start_tf)
+    def test_increment_pos_x(self):
+        print("--- test_increment_pos_x ---")
+        input_start_tf = np.identity(4)
+        print("input start transform: \n", input_start_tf)
 
-print("--- Test Increment Teleop Controller --- \n")
-test_itc()
+        self.ftc.enable(input_start_tf, self.output_start_tf)
+        for _ in range(5):
+            input_start_tf[0,3] = input_start_tf[0,3] + self.inc_x
+            self.ftc.update(input_start_tf)
+        self.ftc.disable()
 
-print("\n")
-print("--- Test Follower Teleop Controller --- \n")
-test_ftc()
+    def test_clutch(self):
+        def track_output_callback(js):
+            print("output joint state to send: \n", js)
+            track_output_callback.tracked_tf = compute_fk(js)
+            print("output transform result: \n", track_output_callback.tracked_tf)
+
+        print("--- test_clutch ---")
+        input_start_tf = np.identity(4)
+        print("input start transform: \n", input_start_tf)
+
+        ftc = FollowTeleopController()
+        ftc.register(track_output_callback)
+        ftc.enable(input_start_tf, self.output_start_tf)
+        for _ in range(5):
+            input_start_tf[0,3] = input_start_tf[0,3] + self.inc_x
+            ftc.update(input_start_tf)
+
+        ftc.clutch()
+        input_start_tf = np.identity(4) # input becomes 0 again
+        print("input clutched back to zero again transform: \n", input_start_tf)
+        print("current output tf when clutched: \n", track_output_callback.tracked_tf)
+
+        # output tf should expect to continue incrementing x
+        # even though input was zeroed
+        ftc.unclutch(input_start_tf, track_output_callback.tracked_tf)
+        for _ in range(5):
+            input_start_tf[0,3] = input_start_tf[0,3] + self.inc_x
+            ftc.update(input_start_tf)
+        ftc.disable()
+
+if __name__ == '__main__':
+    unittest.main()
