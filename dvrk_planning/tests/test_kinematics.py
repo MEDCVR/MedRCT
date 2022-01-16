e2b29308517c389224239e45097a641d7d035366
Radian Gondokaryono <radian.gondokaryono@dorabot.com>
2022-01-16 10:31:36 -0500

    Time the fk and ik functions

--- dvrk_planning/tests/test_kinematics.py
+++ dvrk_planning/tests/test_kinematics.py
@@ -1,10 +1,20 @@
 import dvrk_planning as dp
+from timer import Timer
+
+t = Timer()
 
 joint_pos = [0.0, 0.0, 0.1, 0.0, 0.0, 0.0]
 print("Input joint pos: \n", joint_pos)
 
-output_fk = dp.kinematics.psm.compute_fk(joint_pos)
+output_fk = t.time_average(lambda : dp.kinematics.psm.compute_fk(joint_pos))
 print("FK: \n", output_fk)
+print(t.str_average())
 
-output_jp = dp.kinematics.psm.compute_ik(output_fk)
+output_jp = t.time_average(lambda : dp.kinematics.psm.compute_ik(output_fk))
 print("IK: \n", output_jp)
+print(t.str_average())
+
+t.start()
+output_jp = dp.kinematics.psm.compute_ik(output_fk)
+t.stop()
+print(t.str_elapsed_time())
