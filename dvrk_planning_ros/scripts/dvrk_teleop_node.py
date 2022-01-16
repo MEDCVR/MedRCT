815283ff2b1c5ca769c2072cd583ee5b0ce9390d
Radian Gondokaryono <radian.gondokaryono@dorabot.com>
2022-01-16 14:09:59 -0500

    Wait for subscribers in important places

--- dvrk_planning_ros/scripts/dvrk_teleop_node.py
+++ dvrk_planning_ros/scripts/dvrk_teleop_node.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 
 import argparse
-from re import X
-from tkinter import Y
-import numpy as np
 import os.path
 import rospy
 import rospkg
@@ -15,7 +12,7 @@ from sensor_msgs.msg import JointState, Joy
 
 from PyKDL import Rotation, Vector
 from dvrk_planning.controller.teleop_controller import FollowTeleopController, IncrementTeleopController, ControllerType
-from dvrk_planning_ros.utils import *
+from dvrk_planning_ros.utils import gm_tf_to_numpy_mat
 
 def to_pykdl(quaternion_yaml):
     x = quaternion_yaml["x"]
@@ -29,29 +26,43 @@ class RosTeleopController:
     def __init__(self, controller_yaml):
         self.name = controller_yaml["name"]
         self.output_pub = rospy.Publisher(controller_yaml["output_topic"], JointState, queue_size = 1)
-        self.output_feedback_sub = rospy.Subscriber(controller_yaml["output_feedback_topic"], JointState, self._output_feedback_callback)
+        self.output_feedback_topic = controller_yaml["output_feedback_topic"]
+        self.output_feedback_sub = rospy.Subscriber(self.output_feedback_topic, JointState, self._output_feedback_callback)
 
         output_base_to_camera_rot = Rotation.Quaternion(0, 0, 0, 1)
         output_base_to_camera_rot_str = "output_to_camera_rot"
         if(output_base_to_camera_rot_str in controller_yaml):
             output_base_to_camera_rot = to_pykdl(controller_yaml[output_base_to_camera_rot_str])
 
-        input_topic_name = controller_yaml["input_topic"]
+        self.input_topic = controller_yaml["input_topic"]
         if controller_yaml["type"] == "follow":
             self._teleop_controller = FollowTeleopController(output_to_camera_rot = output_base_to_camera_rot)
-            self.input_sub = rospy.Subscriber(input_topic_name, TransformStamped, self._input_callback_tf)
+            self.input_topic_type = TransformStamped
+            sub_callback = self._input_callback_tf
         elif controller_yaml["type"] == "increment":
             self._teleop_controller = IncrementTeleopController(output_to_camera_rot = output_base_to_camera_rot)
-            self.input_sub = rospy.Subscriber(input_topic_name, Twist, self._input_callback_twist)
+            self.input_topic_type = Twist
+            sub_callback = self._input_callback_twist
         else:
             raise NotImplementedError
+        self.input_sub = rospy.Subscriber(self.input_topic, self.input_topic_type, sub_callback)
 
         self._teleop_controller.register(self._output_callback)
 
-    def wait_subscribers(self):
-        x = 0
+    def _get_str_name(self):
+        return "Teleop controller [" + self.name + "]"
+
+    def _wait_for_subscribers_message(self):
+        print(self._get_str_name(), ": waiting for message from topic [" + self.input_topic +"]" )
+        rospy.wait_for_message(self.input_topic, self.input_topic_type)
+        print(self._get_str_name(), ": finised waiting for message from topic [" + self.input_topic +"]" )
+
+        print(self._get_str_name(), ": waiting for message from topic [" + self.output_feedback_topic +"]" )
+        rospy.wait_for_message(self.output_feedback_topic, self.JointState)
+        print(self._get_str_name(), ": finised for message from topic [" + self.output_feedback_topic +"]" )
 
     def enable(self):
+        self._wait_for_subscribers_message()
         # TODO, this is not good oop
         if self._teleop_controller.controller_type == ControllerType.INCREMENT:
             self._teleop_controller.enable(self.current_output_tf)
@@ -69,6 +80,7 @@ class RosTeleopController:
         if self._teleop_controller.controller_type == ControllerType.INCREMENT:
             self._teleop_controller.unclutch()
         elif self._teleop_controller.controller_type == ControllerType.FOLLOW:
+            self._wait_for_subscribers_message()
             self._teleop_controller.unclutch(self.current_input_tf, self.current_output_tf)
 
     # Output feedback needs a tf, but js is the simplest type of
@@ -109,12 +121,15 @@ class DvrkTeleopNode:
             self.enable_list = switcher_yaml["enable_list"]
             self.switch_sub = rospy.Subscriber(switch_topic, Joy, self.switch_callback)
 
+        for _, rtc in self.ros_teleop_controllers.items():
+            rtc.enable()
+
     def clutch_callback(self, data):
         if data.buttons[0] == 1:
-            for rtc in self.ros_teleop_controllers:
+            for _, rtc in self.ros_teleop_controllers.items():
                 rtc.clutch()
         elif data.buttons[0] == 0:
-            for rtc in self.ros_teleop_controllers:
+            for _, rtc in self.ros_teleop_controllers.items():
                 rtc.unclutch()
 
     def _execute_enable_disable(self, enable_list, disable_list):
