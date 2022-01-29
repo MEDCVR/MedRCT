dc0afc01aed36ce29dde6af67747451a80853e3d
Radian Gondokaryono <radian.gondokaryono@mail.utoronto.ca>
2022-01-29 14:24:20 -0500

    Add statistics input callback average time, refactor to ros teleop controller base class input sub

--- dvrk_planning_ros/src/dvrk_planning_ros/ros_joint_teleop_controller.py
+++ dvrk_planning_ros/src/dvrk_planning_ros/ros_joint_teleop_controller.py
@@ -8,32 +8,24 @@ from dvrk_planning_ros.ros_teleop_controller import RosTeleopController
 
 class RosJointTeleopController(RosTeleopController):
     def __init__(self, controller_yaml):
-        super().__init__(controller_yaml)
 
         input_yaml = controller_yaml["input"]
-        self.input_topic = input_yaml["topic"]
         if input_yaml["type"] == "follow":
             scale = 1.0
             if "scale" in input_yaml:
                 scale = input_yaml["scale"]
-            self._teleop_controller = JointFollowTeleopController(scale)
+            teleop_controller = JointFollowTeleopController(scale)
         elif input_yaml["type"] == "increment":
-            self._teleop_controller = JointIncrementTeleopController()
+            teleop_controller = JointIncrementTeleopController()
         else:
             raise KeyError ("controller: input type: must be follow or increment")
-        self.input_sub = rospy.Subscriber(self.input_topic, JointState, self._input_callback_js)
-        self.current_input_jps = np.array([])
-        self._teleop_controller.register(self._output_callback)
 
-    def _wait_for_input_sub_msg(self, always_print=False):
-        try:
-            if(always_print):
-                 raise
-            rospy.wait_for_message(self.input_topic, JointState, timeout=0.01) # timeout 0.1s to see if publishing
-        except:
-            print(self._get_str_name(), ": waiting for message from topic [" + self.input_topic +"]" )
-            rospy.wait_for_message(self.input_topic, JointState)
-            print(self._get_str_name(), ": finished waiting for message from topic [" + self.input_topic +"]" )
+        self._input_callback_impl = self._input_callback_js
+        self.current_input_jps = np.array([])
+
+        super().__init__(controller_yaml, JointState)
+        self._teleop_controller = teleop_controller
+        self._teleop_controller.register(self._output_callback)
 
     def enable(self):
         self._wait_for_output_feedback_sub_msg(True)
